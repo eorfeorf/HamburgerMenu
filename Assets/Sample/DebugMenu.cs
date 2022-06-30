8d98d79eb4b7dc91358c611a17dfaf4204085989
EF <eorf256@gmail.com>
2022-07-01 03:31:02 +0900

    サンプル変更

--- Assets/Sample/DebugMenu.cs
+++ Assets/Sample/DebugMenu.cs
@@ -52,15 +52,47 @@ namespace Sample
             }).AddTo(this);
             
             hamburgerMenu.Initialize();
-            hamburgerMenu.AddSliderInt("int",10, 0, 50, 10);
-            hamburgerMenu.AddSliderFloat("float",10, 0, 50, 0.1f);
-            hamburgerMenu.AddToggle("toggle", false);
-            hamburgerMenu.AddDropdown<Test>("dropdown", (int) Test.none);
-            hamburgerMenu.AddDropdown("dictionary", dict, 1);
-            hamburgerMenu.AddDropdown("list", list, 0);
-            hamburgerMenu.AddDropdown("array", ary, 0);
-            hamburgerMenu.AddDropdown("array2", ary2, 0);
-            hamburgerMenu.AddTextField("Text", "test text");
+            
+            hamburgerMenu.AddSliderInt("int",10, 0, 50, 10).Subscribe(x =>
+            {
+                Debug.Log("slider int: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddSliderFloat("float",10, 0, 50, 0.1f).Subscribe(x =>
+            {
+                Debug.Log("slider float: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddToggle("toggle", false).Subscribe(x =>
+            {
+                Debug.Log("toggle: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddDropdown<Test>("dropdown", (int) Test.none).Subscribe(x =>
+            {
+                Debug.Log("dropdown: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddDropdown("dictionary", dict, 1).Subscribe(x =>
+            {
+                Debug.Log("dictionary: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddDropdown("list", list, 0).Subscribe(x =>
+            {
+                Debug.Log("list: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddDropdown("array", ary, 0).Subscribe(x =>
+            {
+                Debug.Log("array: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddDropdown("array2", ary2, 0).Subscribe(x =>
+            {
+                Debug.Log("array2: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddTextField("text", "10").Subscribe(x =>
+            {
+                Debug.Log("text field: " + x);
+            }).AddTo(this);
+            hamburgerMenu.AddButton("button").Subscribe(_ =>
+            {
+                Debug.Log("button: pushed");
+            }).AddTo(this);
         }
     }
 }
